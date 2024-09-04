d4d3f292574d372b34076cfbbcedf09750abb39f
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-04 23:07:31 +0600

    first commit

---
+++ logical not.js
@@ -0,0 +1,18 @@
+const isPassed = true ;
+
+if(isPassed === false){
+    console.log('have to take the course again')
+}
+else{
+    console.log('Congratulations')
+}
+
+
+// now using logical not
+
+if(!isPassed){
+    console.log('have to take the course again')
+}
+else{
+    console.log('congratulations')
+}
